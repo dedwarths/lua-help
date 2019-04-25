85b2468efbb3296e0350af7bf8dffe2ee43d7041
dedwarths <49967083+dedwarths@users.noreply.github.com>
2019-04-24 23:25:50 -0300

    Update lua-color-text.lua
    Quem quiser ver outras implementações: https://github.com/dedwarths/lua-help/blob/master/lua-colour-extra

--- lua-color-text.lua
+++ lua-color-text.lua
@@ -1,3 +1,4 @@
+
 textoCor={}
 function adiciona(cor,texto,pretotexto)
   local dict={red={1,0,0},blue={0,0,1},green={0,1,0},yellow={1,1,0},magenta={1,0,1},cyan={0,1,1},gray={0.5,0.5,0.5},white={1,1,1}}
